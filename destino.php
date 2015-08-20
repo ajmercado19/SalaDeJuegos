5bc45c0babd827e3e31132e864bf22969a47a820
octavio <octavio@octaviovillegas.com.ar>
2015-08-20 19:51:32 -0300

    Primer Guardado

---
+++ destino.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump($_REQUEST);
+echo "<br>";
+echo "con request :". $_REQUEST['usuario'];
+echo "<br>";
+echo  "con get :".$_GET['usuario'];
+echo "<br>";
+echo  "con post :".$_POST['usuario'];
+
+
+
+?>
