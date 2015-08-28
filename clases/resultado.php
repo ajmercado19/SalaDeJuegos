357fe61698503263f9b39953f8f9a9f301c28ad7
octavio <el.octavio.villegas@gmail.com>
2015-08-27 21:28:10 -0300

    se creo el método InsertarResultado e InsertarResultadoConParametros en la pagina GuardarResultado.php se hace el testing de estas funciones

--- clases/resultado.php
+++ clases/resultado.php
@@ -14,5 +14,23 @@ class Resultado
             return $consulta->fetchAll(PDO::FETCH_CLASS, "Resultado"); 
 
    }
+    public function InsertarResultadoConParametros()
+     {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into resultado (usuario,juego,resultado)values(:paramUsuario,:paramJuego,:paramResultado)");
+                $consulta->bindValue(':paramUsuario',$this->usuario, PDO::PARAM_INT);
+                $consulta->bindValue(':paramJuego', $this->juego, PDO::PARAM_STR);
+                $consulta->bindValue(':paramResultado', $this->resultado, PDO::PARAM_STR);
+                $consulta->execute();       
+                return $objetoAccesoDato->RetornarUltimoIdInsertado();
+     }
+     public function InsertarResultado()
+     {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into resultado (usuario,juego,resultado)values('$this->usuario','$this->juego','$this->resultado')");
+                
+                $consulta->execute();       
+                return $objetoAccesoDato->RetornarUltimoIdInsertado();
+     }
 }
 ?>
